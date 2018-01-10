b336314ddf1f5452da838aded1ec1658dedfa6ef
Guy Melul <guymalul95@gmail.com>
2018-01-10 14:33:34 +0200

    game done lul

---
+++ Assets/Abbadon/Player/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public ParticleSystem muzzleFlashParticles;
+
+    // Use this for initialization
+    void Start () {
+        muzzleFlashParticles.Stop();
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // Get Input (Fire weapon, etc...)
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }   
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward.normalized * 10;
+
+        muzzleFlashParticles.Play();
+
+        // Destroy the bullet after 5 seconds or later if it collides
+        Destroy(bullet, 5.0f);
+    }
+}
