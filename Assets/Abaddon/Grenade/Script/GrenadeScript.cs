a27c2f2d49faa8b4179c254c13b85a8e9052346f
Guy Melul <guymalul95@gmail.com>
2018-02-02 17:07:45 +0200

    shit load of changes

---
+++ Assets/Abaddon/Grenade/Script/GrenadeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GrenadeType
+{
+	Timer,
+	Impact
+}
+public class GrenadeScript : MonoBehaviour {
+
+	public GameObject ExplosionPrefab;
+	public GrenadeType GrenadeType;
+	public int GrenadeTimeMS;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update for timer
+	void Update()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter(Collision collision)
+    {
+		if(GrenadeType != GrenadeType.Impact) return;
+
+        if(collision.gameObject.tag != "Player")
+		{
+
+		}
+    }
+}
