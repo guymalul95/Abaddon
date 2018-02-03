5ee7d5f53b511e7986c30ed899c73cfeb63204c7
John Lahav <yoni9093@gmail.com>
2018-02-03 22:25:31 +0200

    weapon system undone

---
+++ Assets/Abaddon/Player/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject weaponPrefab;
+    private float health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void SwitchWeapon(GameObject newWeaponPrefab)
+    {
+        this.weaponPrefab = newWeaponPrefab;
+    }
+}
