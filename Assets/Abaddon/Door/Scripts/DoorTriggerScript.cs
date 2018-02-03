f06f3919f3526c8efaa8e1084614dc43506503bc
Guy Melul <guymalul95@gmail.com>
2018-02-04 01:01:34 +0200

    add montesrs

--- Assets/Abaddon/Door/Scripts/DoorTriggerScript.cs
+++ Assets/Abaddon/Door/Scripts/DoorTriggerScript.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class DoorTriggerScript : MonoBehaviour {
     private Animator animator;
+    private AudioSource audioSource;
+    public AudioClip DoorOpenAudio;
+    public AudioClip DoorCloseAudio;
     private const string PlayerTag = "Player";
 
     // Use this for initialization
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = false;
     }
 
     void OnTriggerEnter(Collider other)
@@ -18,6 +24,9 @@ public class DoorTriggerScript : MonoBehaviour {
         if (PlayerTag != other.tag) return;
         animator.ResetTrigger("DoorCloseTrigger");
         animator.SetTrigger("DoorOpenTrigger");
+
+        audioSource.clip = DoorOpenAudio;
+        audioSource.Play();
     }
 
     void OnTriggerExit(Collider other)
@@ -25,5 +34,8 @@ public class DoorTriggerScript : MonoBehaviour {
         if (PlayerTag != other.tag) return;
         animator.ResetTrigger("DoorOpenTrigger");
         animator.SetTrigger("DoorCloseTrigger");
+
+        audioSource.clip = DoorCloseAudio;
+        audioSource.PlayDelayed(0.5f);
     }
 }
---
+++ Assets/Abaddon/Zombie/Scripts/ZombieController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour {
+    public float WalkSpeed;
+    private Vector3 WalkVector;
+
+	// Use this for initialization
+	void Start () {
+        WalkVector = new Vector3(0, 0, WalkSpeed);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Translate(WalkVector * Time.deltaTime);
+    }
+}
